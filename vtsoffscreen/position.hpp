a964dadab733a82a04f58a469059d2dd345cd8e0
Vaclav Blazek <vaclav.blazek@citationtech.net>
2018-02-23 16:29:25 +0100

    added OSS comments

--- vtsoffscreen/position.hpp
+++ vtsoffscreen/position.hpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2017 Melown Technologies SE
+ * Copyright (c) 2018 Melown Technologies SE
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
